61a8d9f0cd1e715e1b956bc98114d47725570e53
Alex <alex@gu3.me>
2022-06-10 02:46:18 +0100

    Basic kill message HUD

---
+++ code/ui/hud/KillMessages.cs
@@ -0,0 +1,28 @@
+﻿namespace OpenArena;
+
+[UseTemplate]
+public class KillMessages : Panel
+{
+	class Message : Panel
+	{
+		public Message( string victimName )
+		{
+			Add.Label( $"You fragged {victimName}", "small" );
+			_ = LiveFor( 3.0f );
+		}
+
+		private async Task LiveFor( float seconds )
+		{
+			await Task.DelaySeconds( seconds );
+			this.Delete();
+		}
+	}
+
+	[ArenaEvent.Player.Kill]
+	public void OnKill( Player victim )
+	{
+		Log.Trace( $"KM: killed player {victim.Client.Name}" );
+		var message = new Message( victim.Client.Name );
+		message.Parent = this;
+	}
+}
