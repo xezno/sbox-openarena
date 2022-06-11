2e08d34722719b1c55243b6fbb8c133dd8307757
Alex <alex@gu3.me>
2022-06-11 03:04:20 +0100

    Draw player speed just below crosshair

---
+++ code/ui/hud/Speed.cs
@@ -0,0 +1,7 @@
+﻿namespace OpenArena;
+
+[UseTemplate]
+internal class Speed : Panel
+{
+	public string PlayerSpeed => $"{MathF.Abs( Local.Pawn.Velocity.Dot( Local.Pawn.EyeRotation.Forward ) ).FloorToInt()} u/s";
+}
