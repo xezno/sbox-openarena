181bd84261af33c19583dc9063956f8adad080d7
Alex <alex@gu3.me>
2022-06-16 23:56:21 +0100

    Remove key overlay

--- code/Game.cs
+++ code/Game.cs
@@ -72,62 +72,7 @@ public partial class ArenaGame : Sandbox.Game
 		using ( Render.Draw2D.MatrixScope( matrix ) )
 		{
 			player.RenderHud( screenSize );
-			DrawKeyOverlay( screenSize );
 		}
 	}
-
-	private void DrawKeyOverlay( Vector2 screenSize )
-	{
-		var draw = Render.Draw2D;
-		void DrawInputKey( Vector2 pos, InputButton button, Vector2? size = null )
-		{
-			var key = Input.GetButtonOrigin( button ).ToUpper();
-			var pressed = Input.Down( button );
-			size ??= new Vector2( 64, 64 );
-
-			//
-			// Draw box
-			//
-			var corners = new Vector4( 4 );
-			var boxColor = pressed ? Color.White
-								   : Color.White.WithAlpha( 0.1f );
-			var boxBorderColor = pressed ? Color.White
-										 : Color.White.WithAlpha( 0.2f );
-
-			// This is obsolete, but what do we replace it with?
-			draw.BoxWithBorder( new Rect( pos, size ?? default ), boxColor, 2f, boxBorderColor, corners );
-
-			//
-			// Draw text
-			//
-			var textSize = draw.TextSize( pos, key ).Size;
-			var textCenter = ( ( size ?? default ) - textSize ) / 2.0f;
-
-			draw.FontFamily = "Rajdhani";
-			draw.FontWeight = 800;
-			draw.Color = pressed ? Color.Black : Color.White;
-			draw.Text( pos + textCenter, key );
-		}
-
-		Vector2 margins = new( 8, 8 );
-		Vector2 start = new( screenSize.x - 64 - ( 64 + margins.x ) * 3, screenSize.y - 64 - ( 64 + margins.y ) * 3 );
-		Vector2 position = start;
-
-		position += new Vector2( 64, 0 ) + margins.WithY( 0 );
-		DrawInputKey( position, InputButton.Forward );
-
-		position += new Vector2( -64, 64 ) + margins.WithX( -margins.x );
-		DrawInputKey( position, InputButton.Left );
-
-		position += new Vector2( 64, 0 ) + margins.WithY( 0 );
-		DrawInputKey( position, InputButton.Back );
-
-		position += new Vector2( 64, 0 ) + margins.WithY( 0 );
-		DrawInputKey( position, InputButton.Right );
-
-		position = start + new Vector2( 0, 128 ) + new Vector2( 0, margins.y * 2 );
-		DrawInputKey( position, InputButton.Jump, new Vector2( 192 + margins.x * 2, 64 ) );
-	}
-
 }
 
