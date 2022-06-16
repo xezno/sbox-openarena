7c382c8da0425389dce4f953eb80b52502fb0d02
Alex <alex@gu3.me>
2022-06-16 13:57:46 +0100

    Add melee weapon

---
+++ code/weapons/Crowbar.cs
@@ -0,0 +1,65 @@
+﻿namespace OpenArena;
+
+[Library( "oa_weapon_crowbar" )]
+public class Crowbar : BaseWeapon
+{
+	public override void CreateViewModel()
+	{
+		base.CreateViewModel();
+
+		ViewModelEntity?.SetAnimGraph( "models/first_person/first_person_arms_punching.vanmgrph" );
+	}
+
+	public override void AttackPrimary()
+	{
+		SwingCrowbar();
+	}
+
+	private TraceResult TraceCrowbarSwing()
+	{
+		Vector3 start = Owner.EyePosition;
+		Vector3 end = Owner.EyePosition + Owner.EyeRotation.Forward * 64f;
+
+		float radius = 2.0f;
+
+		bool inWater = Map.Physics.IsPointWater( start );
+
+		var tr = Trace.Ray( start, end )
+				.UseHitboxes()
+				.HitLayer( CollisionLayer.Water, !inWater )
+				.HitLayer( CollisionLayer.Debris )
+				.Ignore( Owner )
+				.Ignore( this )
+				.Size( radius )
+				.Run();
+
+		return tr;
+	}
+
+	private void SwingCrowbar()
+	{
+		var tr = TraceCrowbarSwing();
+
+		//
+		// Swing damage etc.
+		//
+		if ( tr.Hit )
+		{
+			tr.Surface.DoBulletImpact( tr );
+			if ( tr.Entity.IsValid() && !tr.Entity.IsWorld )
+			{
+				tr.Entity.TakeDamage( CreateDamageInfo( tr ) );
+			}
+		}
+
+		//
+		// Swing effects
+		//
+		CreateSwingEffects();
+	}
+
+	private void CreateSwingEffects()
+	{
+		ViewModelEntity?.SetAnimParameter( "attack", true );
+	}
+}
